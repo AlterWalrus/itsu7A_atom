c212453a1d7f2433d2911e7430283e54cf064697
AlterWalrus <suraeruarroyo@gmail.com>
2025-11-23 12:23:38 -0600

    Wifi
    Esta implementación funciona por un rato y luego muere, el potencial culpable es la fragmentación de memoria por usar Strings dinámicos

--- atom1.ino
+++ atom1.ino
@@ -1,4 +1,6 @@
 #include "BluetoothSerial.h"
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -18,7 +20,7 @@
 
 #define BUZZER 19
 
-//trig -> 5, echo -> 17, 5v
+//This thing uses 5v
 #define SOUND_SPEED_2 0.017 //Sound speed / 2
 #define TRIG_PIN 5
 #define ECHO_PIN 17
@@ -28,6 +30,10 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 BluetoothSerial serialBT;
 int led_state = LOW;
 int buzz_state = LOW;
+
+AsyncWebServer server(80);
+String ip;
+bool connecting_wifi = false;
 
 const int freq = 30000;
 const int pwmChannel = 0;
@@ -52,7 +58,24 @@ void setup(){
   pinMode(ECHO_PIN, INPUT);
 
   //Bluetooth
-  serialBT.begin("AtomV1");  
+  serialBT.begin("AtomV1");
+
+  //Wifi
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
+  server.on("/data", HTTP_GET, [](AsyncWebServerRequest *request){
+    char buffer[64]; 
+    int lectura = random(0, 50); 
+    snprintf(buffer, sizeof(buffer), "{\"prox\":%d}", lectura);
+    request->send(200, "application/json", buffer);
+  });
+  /*
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
+  server.on("/data", HTTP_GET, [](AsyncWebServerRequest *request){
+      String json = "{\"prox\":" + String(random(10, 50)) + ", \"estado\":\"online\"}";
+      request->send(200, "application/json", json);
+  });
+  */
+  
 
   //Motor
   pinMode(MOTOR_IN1, OUTPUT);
@@ -77,17 +100,31 @@ void setup(){
 
   display.setTextSize(2);
   display.setTextColor(WHITE);
-  display.setCursor(0, 10);
 }
 
 void loop(){
   if(Serial.available()){
     String data = Serial.readString();
-    serialBT.print(data);
-    printOLED(data);
+    data.trim();
+    //serialBT.print(data);
+    //printOLED(data);
+
   }
   
   if(serialBT.available()){
+    if(connecting_wifi){
+      String data = serialBT.readString();
+      int comma = data.indexOf(",");
+
+      String ssid = data.substring(1, comma);
+      String pass = data.substring(comma + 1, data.length()-1);
+      Serial.println(ssid);
+      Serial.println(pass);
+      connectWiFi(ssid, pass);
+      connecting_wifi = false;
+      return;
+    }
+
     char data = serialBT.read();
 
     //Message reading
@@ -99,6 +136,12 @@ void loop(){
       }
 
       //Message read, do something with it
+      if(text_buffer == "wifi"){
+        printOLED("Esperando red...");
+        connecting_wifi = true;
+        return;
+      }else
+
       if(text_buffer == "led"){
         led_state = !led_state;
         digitalWrite(2, led_state);
@@ -112,12 +155,31 @@ void loop(){
     //Commands
     if(data == 'p'){
       automatic = !automatic;
+      Serial.println(automatic ? "AUTOMATIC ACTIVATED" : "AUTOMATIC DEACTIVATED");
+      beep(automatic ? 2 : 1);
     }else
 
     if(data == 'b'){
       buzz_state = !buzz_state;
       digitalWrite(BUZZER, buzz_state);
       Serial.println(buzz_state ? "BUZZER ON" : "BUZZER OFF");
+    }else
+
+    if(data == ':'){
+      char c = serialBT.read();
+      Serial.println("EMOTE CHANGED");
+      if(c == ')'){
+        drawHappy();
+      }else
+      if(c == '('){
+        drawSad();
+      }else
+      if(c == 'o'){
+        drawWow();
+      }else
+      if(c == 'v'){
+        drawPacMan();
+      }
     }else
 
     if(data == 'e'){
@@ -170,12 +232,30 @@ void loop(){
       setMotors(HIGH, LOW, LOW, HIGH);
     }else{
       //Rotate
-      setSpeed(2);
+      setSpeed(210);
       setMotors(LOW, HIGH, LOW, HIGH);
     }
   }
   
-  delay(32);
+  //delay(32);
+  vTaskDelay(32 / portTICK_PERIOD_MS);
+}
+
+void connectWiFi(String ssid, String pass){
+  WiFi.begin(ssid.c_str(), pass.c_str());
+  for(int i = 0; i < 30; i++){
+    if(WiFi.status() == WL_CONNECTED){
+      server.begin();
+      ip = WiFi.localIP().toString();
+      serialBT.println("IP: " + ip);
+      printOLED(ip);
+      beep(3);
+      return;
+    }
+    delay(500);
+  }
+  printOLED("Error al  conectar  :(");
+  Serial.println("Error al  conectar  :(");
 }
 
 void setSpeed(int speed){
@@ -184,18 +264,63 @@ void setSpeed(int speed){
 }
 
 void setMotors(bool m1, bool m2, bool m3, bool m4){
-  if(automatic){
-    return;
-  }
   digitalWrite(MOTOR_IN1, m1);
   digitalWrite(MOTOR_IN2, m2);
   digitalWrite(MOTOR_IN3, m3);
   digitalWrite(MOTOR_IN4, m4);
 }
 
+void beep(int times){
+  for(int i = 0; i < times; i++){
+    digitalWrite(BUZZER, HIGH);
+    delay(100);
+    digitalWrite(BUZZER, LOW);
+    delay(100);
+  }
+}
+
 void printOLED(String msg){
   display.clearDisplay();
-  display.setCursor(0, 0);
+  display.setCursor(0, 8);
   display.print(msg);
+  display.display();
+}
+
+void drawHappy(){
+  Serial.println("drawing happy-face");
+  display.clearDisplay();
+  int y = 1;
+  for(int i = 0; i < 16; i++){
+    display.fillCircle(32+(i*4), 32+y, 4, WHITE);
+    y += (i < 8 ? 1 : -1);
+  }
+  display.display();
+}
+
+void drawSad(){
+  Serial.println("drawing sad-face");
+  display.clearDisplay();
+  int y = 1;
+  for(int i = 0; i < 16; i++){
+    display.fillCircle(32+(i*4), 32+y, 4, WHITE);
+    y += (i < 8 ? -1 : 1);
+  }
+  display.display();
+}
+
+void drawWow(){
+  Serial.println("drawing wow-face");
+  display.clearDisplay();
+  display.fillCircle(64, 32, 16, WHITE);
+  display.display();
+}
+
+void drawPacMan(){
+  Serial.println("drawing puck-face");
+  display.clearDisplay();
+  for(int i = 0; i < 16; i++){
+    display.fillCircle(32+(i*4), 32-i, 4, WHITE);
+    display.fillCircle(32+(i*4), 32+i, 4, WHITE);
+  }
   display.display();
 }
